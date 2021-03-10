6206b57a17cf9ad3216842ccbb05bfcd8c636bbf
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-03-10 01:32:21 +0100

    Added AddEffect method to GrayToolkit.

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package imagene
 
-import "io"
+import (
+	"image"
+)
 
 // GrayToolkit is the interface that describes a gray manipulation
 // toolkit object.
-type GrayToolkit interface{}
+type GrayToolkit interface {
+	AddEffect() image.Image
+}
 
 // grayToolkit is the object that satisfies the GrayToolki interface.
 type grayToolkit struct {
-	image io.Reader
+	image image.Image
 }
 
 // NewGrayToolkit returns a new gray manipulation toolkit object that
 // implements the GrayToolkit interface.
-func NewGrayToolkit(image io.Reader) GrayToolkit {
+func NewGrayToolkit(image image.Image) GrayToolkit {
 	return &grayToolkit{
 		image: image,
 	}
+}
+
+// AddEffect adds a gray effect to the image and returns the new image
+// that has the gray effect.
+func (gt *grayToolkit) AddEffect() image.Image {
+	grayImage := image.NewGray(gt.image.Bounds())
+	for y := gt.image.Bounds().Min.Y; y < gt.image.Bounds().Max.Y; y++ {
+		for x := gt.image.Bounds().Min.X; x < gt.image.Bounds().Max.X; x++ {
+			grayImage.Set(x, y, gt.image.At(x, y))
+		}
+	}
+	return grayImage
 }
