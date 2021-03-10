dc7fb030abaab2a84687974a972271acb612a830
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-03-10 01:24:40 +0100

    Added some basic types to imagene package for gray manipulation.

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package imagene
+
+import "io"
+
+// GrayToolkit is the interface that describes a gray manipulation
+// toolkit object.
+type GrayToolkit interface{}
+
+// grayToolkit is the object that satisfies the GrayToolki interface.
+type grayToolkit struct {
+	image io.Reader
+}
+
+// NewGrayToolkit returns a new gray manipulation toolkit object that
+// implements the GrayToolkit interface.
+func NewGrayToolkit(image io.Reader) GrayToolkit {
+	return &grayToolkit{
+		image: image,
+	}
+}
